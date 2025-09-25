38ebf21138a062877fdef6a448103af93597ca65
srahi904 <shashiranjanrahi904@gmail.com>
2025-09-25 19:34:58 +0530

    update code

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,10 +32,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  // Fallback for all other routes to serve the index.html
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
   });
 }
 
